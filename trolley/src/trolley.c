0eaad46df1a640f12345542c1fbd8ffa02e11259
Dimitris Zacharatos <sdi2400048@di.uoa.gr>
2024-10-23 20:23:38 +0300

    Working on it

--- trolley/src/trolley.c
+++ trolley/src/trolley.c
@@ -2,4 +2,17 @@
 #include <stdio.h>
 
 int main() {
+int cl;
+int cr;
 printf ("give cost of going left");
+scanf ("%d", &cl);
+printf ("give cost of going right");
+scanf ("%d", &cr);
+while (cl==EOF || cr==EOF){
+ printf ("give cost of going left");
+scanf ("%d", &cl);
+printf ("give cost of going right");
+scanf ("%d", &cr);
+}
+return 0;
+}
