3b7cd7a8cca909495e9bfc6bb48c96a5d12ccdcd
Dimitris Zacharatos <sdi2400048@di.uoa.gr>
2024-10-24 13:39:14 +0300

    Working on it

--- trolley/src/trolley.c
+++ trolley/src/trolley.c
@@ -8,11 +8,16 @@ printf ("give cost of going left");
 scanf ("%d", &cl);
 printf ("give cost of going right");
 scanf ("%d", &cr);
-while (cl==EOF || cr==EOF){
+while (cl!=EOF | cr!=EOF){
+ if (cl<=cr) {
+   printf ("go left\n");
+ }else{ 
+   printf ("go right\n");
+ }
  printf ("give cost of going left");
-scanf ("%d", &cl);
-printf ("give cost of going right");
-scanf ("%d", &cr);
+ scanf ("%d", &cl);
+ printf ("give cost of going right");
+ scanf ("%d", &cr);
 }
 return 0;
 }
