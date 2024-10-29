dfdc8a89542d9c1eaad44b2dd6c773d4952dbbf4
Dimitris Zacharatos <sdi2400048@di.uoa.gr>
2024-10-29 15:59:33 +0200

    Working on it

--- trolley/src/trolley.c
+++ trolley/src/trolley.c
@@ -6,18 +6,29 @@ long long int cl;
 long long int cr;
 printf ("give cost of going left:");
 scanf ("%lld", &cl);
+if (cl==50){
+ return 0;
+}
 printf ("give cost of going right:");
 scanf ("%lld", &cr);
-while (cl!=EOF | cr!=EOF){
+if (cr==50){
+ return 1;
+}
+while (cl!=50 && cr!=50) {
  if (cl<=cr) {
    printf ("go left\n");
  }else{ 
    printf ("go right\n");
- }
- printf ("give cost of going left:");
+ } 
+printf ("give cost of going left:");
  scanf ("%lld", &cl);
+if (cl==50){
+ return 0;
+}
  printf ("give cost of going right:");
  scanf ("%lld", &cr);
+if (cr==50){
+ return 1;
 }
-return 1;
+}
 }
